7328c4e719307eb9e85e4b940ed38845885a330d
Constantine Krotov <ckwebmast@gmail.com>
2022-07-30 17:42:08 +0300

    Completed lesson 2

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let title
-let screens
-let screenPrice
-let rollback
-let fullPrice
-let adaptive
+let title = 'project01'
+let screens = 'Простые, Сложные, Интерактивные'
+let screenPrice = 10
+let rollback = 50
+let fullPrice = 100
+let adaptive = true
 
-console.log('Bye Bye Kitty!')
-alert('Hello Kity!')
+console.log(typeof title)
+console.log(typeof fullPrice)
+console.log(typeof adaptive)
+console.log(screens.length)
+console.log('"Стоимость разработки сайта' + ' ' + screenPrice + ' ' + 'рублей/долларов/гривен/юани"' + ' ' + 'и "Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей/долларов/гривен/юани"')
+console.log(screens.toLowerCase().split(","))
+console.log(fullPrice * rollback / 100)
+
