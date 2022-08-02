60593590b1a8568c9de4cf3a993b39f308b0a692
Constantine Krotov <ckwebmast@gmail.com>
2022-08-03 00:07:32 +0300

    completed lesson03Hard

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const lang = (language) => {
-  if (language === 'ru') {
-    console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье')
-  }
-  else if (language === 'en')
-  console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
-}
+// const lang = (language) => {
+//   if (language === 'ru') {
+//     console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье')
+//   }
+//   else if (language === 'en')
+//   console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+// }
 
-lang('ru')
+// lang('ru')
 
 
 const namePerson = (name) => {
-  name === 'Артем' ? console.log('директор') : console.log('студент')
-  name === 'Александр' ? console.log('преподаватель') : console.log('студент')
+  name === 'Артем' ? console.log('директор') : (name === 'Александр' ? console.log('преподаватель') : console.log('студент'))
+ 
+  
 }
 
-namePerson('Александр')
+namePerson('Артем')
